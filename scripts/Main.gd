ce41fb7491dab234522fa9339c7de5b06f0f587e
Julen Leizza <julenneakaill@gmail.com>
2025-10-20 20:40:30 -0300

    agregue contador de distancia

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -3,14 +3,16 @@ extends Node
 
 const START_SPEED = 10
 const MAX_SPEED = 25
+const DIST_MODIFIER = 10
 
-
+var traveled_distance : int 
 var speed : float
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	traveled_distance = 0
 
 
 
@@ -20,4 +22,9 @@ func _process(delta):
 	$Bondi.position.x += speed
 	$Camera2D.position.x += speed
 
-	pass
+	traveled_distance += int(speed / DIST_MODIFIER)
+
+	show_distance()
+
+func show_distance():
+	$HUD.get_node("DistanceLabel").text = "Distancia recorrida: " + str(traveled_distance)
