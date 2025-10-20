9d0615a750b42f07492aadbf1904bd159415dc08
Julen Leizza <julenneakaill@gmail.com>
2025-10-20 19:29:00 -0300

    Migracion de proyecto a la nueva estructura limpia de Godot.

--- scripts/Bondi.gd
+++ scripts/Bondi.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 
 var move = 50
 var initial_posisition = 410
+var speed = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,7 +12,11 @@ func _ready():
 
 
 func _physics_process(delta):
+
+	$Bondi.position.x += speed	
+
 	if Input.is_action_just_pressed("ui_up") && position.y != initial_posisition :
 		position.y -= move
 	if Input.is_action_just_pressed("ui_down") && position.y != initial_posisition + move:
 		position.y += move
+
