86d5aa7f92055d8da406e1343ebf8649221690a6
Julen Leizza <julenneakaill@gmail.com>
2025-10-19 22:11:47 -0300

    added parallax bg and obstacule scene

--- Bondi.gd
+++ Bondi.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
 
-var move = 100
-var initial_posisition = 241
+var move = 50
+var initial_posisition = 410
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,5 +13,5 @@ func _ready():
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_up") && position.y != initial_posisition :
 		position.y -= move
-	if Input.is_action_just_pressed("ui_down") && position.y != initial_posisition + move * 2:
+	if Input.is_action_just_pressed("ui_down") && position.y != initial_posisition + move:
 		position.y += move
