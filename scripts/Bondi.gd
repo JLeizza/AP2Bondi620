1be243ef55598be2d85a9a80f05a8e36526f6d7d
Julen Leizza <julenneakaill@gmail.com>
2025-11-01 18:45:08 -0300

    Agregue sprites, y colision con objetos en la escena.

--- scripts/Bondi.gd
+++ scripts/Bondi.gd
@@ -6,6 +6,7 @@ var move = 50
 var initial_position = 410 
 var speed 
 var traveled_distance : int 
+var lifes : int = Cte.VIDAS_BONDI
 
 func _ready():
 	position.y = initial_position
@@ -26,3 +27,13 @@ func _physics_process(_delta):
 
 	if Input.is_action_just_pressed("ui_down") && position.y != initial_position + move:
 		position.y += move
+		
+func take_damage(damage):
+	lifes = self.lifes
+	lifes -= damage
+	
+	print("Me la pegué, me hicieron " + str(damage) + " de daño, y tengo " + str(lifes) + " vidas")
+#	if lifes >=0 :
+#		return 
+	
+	
