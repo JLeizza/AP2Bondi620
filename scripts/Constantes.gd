0b6a4740c7f5b2a18c3af010efcd5834ebe6ee47
Julen Leizza <julenneakaill@gmail.com>
2025-11-01 18:45:08 -0300

    Agregue sprites, y colision con objetos en la escena.

--- scripts/Constantes.gd
+++ scripts/Constantes.gd
@@ -4,8 +4,14 @@ const START_SPEED = 5
 const MAX_SPEED = 25
 const DIST_MODIFIER = 10
 const SPEED_MODIFIER = 500
-const SPAWN_OFFSET_X = 800  # Píxeles adelante del bondi
+const VIDAS_BONDI = 4
 
-const SANT_SPAWN_TIME = 12
+#OFFSETS
+const SPAWN_OFFSET_X = 800  # Píxeles adelante del bondi
+const CAMERA_OFFSET_X = -300
+
+##OBSTACULOS Y SANTUARIOS
+const SANT_SPAWN_TIME = 12 #Este tiempo es para debug mas facil, en el juego final diria que sea aprox el doble
 const OBS_SPAWN_TIME = 10
+const DAÑO_OBSTACULO = 1
 
